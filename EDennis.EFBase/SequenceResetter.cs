cf49943709bcca1084faa52a435a83faac719e26
Dennis Mitchell <den.mitchell@yahoo.com>
2018-11-08 12:04:26 -0500

    Fixed a bug in SequenceResetter, which had issues when two sequences were named similarly.

--- EDennis.EFBase/SequenceResetter.cs
+++ EDennis.EFBase/SequenceResetter.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.SqlServer.Management.Common;
-using Microsoft.SqlServer.Management.Smo;
-using System.Data.SqlClient;
 
 namespace EDennis.EFBase {
 
@@ -35,40 +29,77 @@ namespace EDennis.EFBase {
 
         private static string sql =
 @"
+	declare @SequenceSchema varchar(255), @TableSchema varchar(255)
 	declare @SequenceName varchar(255), @TableName nvarchar(255), @ColumnName nvarchar(255)
 	declare @NextValueSql nvarchar(max), @SqlParamDef nvarchar(max), @NextValue int
 
 	set @SqlParamDef = N'@NextValue int OUTPUT'
 
+	declare @seqMax table(
+		SequenceSchema varchar(255),
+		SequenceName varchar(255),
+		MaxValue int
+	);
+
 	declare @ResetSql nvarchar(max)
 
-	declare crsr cursor for
+--
+-- Get all sequence values used as defaults for columns, and use dynamic SQL to
+-- calculate the maximum value of columns using that sequence as a default
+--
+	declare c_seqtab cursor for
 	select 
-		s.sequence_name SequenceName, c.table_name TableName, c.column_name ColumnName,
-		'select @NextValue = max([' + c.column_name + ']) + 1 from [' + c.table_name + ']' NextValueSql
+		s.sequence_schema SequenceSchema, s.sequence_name SequenceName, 
+		c.table_schema TableSchema, c.table_name TableName, c.column_name ColumnName,
+		'select @NextValue = max([' + c.column_name + ']) + 1 from [' + c.table_schema + '].[' + c.table_name + ']' NextValueSql
 		from INFORMATION_SCHEMA.COLUMNS c
 		inner join INFORMATION_SCHEMA.SEQUENCES s
-			on rtrim(replace(replace(replace(replace(c.COLUMN_DEFAULT,'[',''),']',''),')',''),'(','')) LIKE '%next value for%' + s.SEQUENCE_NAME + '%' 
-
-	open crsr
-	while (1=1)
+			on rtrim(replace(replace(replace(replace(replace(c.COLUMN_DEFAULT,'[',''),']',''),')',''),'(',''),'dbo',''))
+				LIKE '%' + 
+				case when s.sequence_schema <> 'dbo' then s.sequence_schema + '.' else '' end +  s.SEQUENCE_NAME 
+	open c_seqtab
+	fetch next from c_seqtab into @SequenceSchema, @SequenceName, @TableSchema, @TableName, @ColumnName, @NextValueSql
+	while @@FETCH_STATUS = 0
 	begin
-		fetch next from crsr into @SequenceName, @TableName, @ColumnName, @NextValueSql
-		if @@FETCH_STATUS <> 0 
-			break
+		if @SequenceSchema is not null
+		begin
 
-		exec sp_executesql	@NextValueSql, 
-							@SqlParamDef,
-							@NextValue = @NextValue OUTPUT;
-
-		set @ResetSql = 'alter sequence [' + @SequenceName + '] restart with ' + convert(varchar,ISNULL(@NextValue,1))
-	
-		exec sp_executesql @ResetSql;
-	
+			exec sp_executesql	@NextValueSql, 
+								@SqlParamDef,
+								@NextValue = @NextValue OUTPUT;
+			insert into @seqMax (SequenceSchema, SequenceName, MaxValue)
+				values (@SequenceSchema, @SequenceName, ISNULL(@NextValue,1))
+		end
+		fetch next from c_seqtab into @SequenceSchema, @SequenceName, @TableSchema, @TableName, @ColumnName, @NextValueSql	
 	end
+	close c_seqtab
+	deallocate c_seqtab
 
-	close crsr
-	deallocate crsr
+--
+-- Because sequences may be used by more than one table, get the maximum value across all usages,
+-- and use that value to reset the sequence
+--
+	declare @MaxValue int
+
+	declare c_seqmax cursor for
+		select SequenceSchema, SequenceName, max(MaxValue) MaxValue
+		from @seqMax
+		group by SequenceSchema, SequenceName
+
+	open c_seqmax
+	fetch next from c_seqmax into @SequenceSchema, @SequenceName, @MaxValue
+	while @@fetch_status = 0
+	begin
+		if @SequenceSchema is not null
+		begin
+			set @ResetSql = 'alter sequence [' + @SequenceSchema + '].[' + @SequenceName + '] restart with ' + convert(varchar,ISNULL(@MaxValue,1))	
+			exec sp_executesql @ResetSql;			
+		end
+		fetch next from c_seqmax into @SequenceSchema, @SequenceName, @MaxValue
+	end
+	close c_seqmax
+	deallocate c_seqmax
+
 
 ";
     }
